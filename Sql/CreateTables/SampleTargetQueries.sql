c9d7ba6de1f14c98ca40fe8812789a93c711bbbf
Andrew Hunter <andrew.hunter@red-gate.com>
2014-03-25 14:49:46 +0000

    Ah, now I see why the queries are so complicated for cerebrata. SQL is definitely not going to scale in the long term, I think.

--- Sql/CreateTables/SampleTargetQueries.sql
+++ Sql/CreateTables/SampleTargetQueries.sql
@@ -10,6 +10,7 @@ WHERE	ep.PropertyName = 'EventType' AND ep.Value = 'Start'
 GO
 
 -- builder.AllEvents().Only("EventType", "Start").BrokenDownBy("Day");
+-- This is the simple way but the results don't contain zeros for days with no events
 SELECT		ep2.Value as Day, COUNT(DISTINCT ep1.EventId) 
 FROM		EventPropertyValues AS ep1
 INNER JOIN	EventPropertyValues as ep2 ON (ep2.EventId = ep1.EventId AND ep1.PropertyName = 'EventType' AND ep1.Value = 'Start' AND ep2.PropertyName = 'Day')
@@ -17,6 +18,7 @@ GROUP BY	ep2.Value
 GO
 
 -- builder.AllEvents().Only("EventType", "DoSomething").BrokenDownBy("Day");
+-- Illustrates the problem outlined above, day 3 is missing
 SELECT		ep2.Value as Day, COUNT(DISTINCT ep1.EventId) 
 FROM		EventPropertyValues AS ep1
 INNER JOIN	EventPropertyValues as ep2 ON (ep2.EventId = ep1.EventId AND ep1.PropertyName = 'EventType' AND ep1.Value = 'DoSomething' AND ep2.PropertyName = 'Day')
