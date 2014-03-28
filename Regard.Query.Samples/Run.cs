e842f914b98f527e26408cc4d1a06553e188beb3
Andrew Hunter <andrew.hunter@red-gate.com>
2014-03-28 15:44:47 +0000

    Alter the test app so that it actually connects to the database and tries out the query

--- Regard.Query.Samples/Run.cs
+++ Regard.Query.Samples/Run.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 using Regard.Query.Api;
 using Regard.Query.Sql;
 
@@ -9,22 +11,43 @@ namespace Regard.Query.Samples
         [STAThread]
         public static void Main()
         {
-            Console.WriteLine(@"builder.AllEvents().Only(""EventType"", ""DoSomething"").CountUniqueValues(""SessionId"").BrokenDownBy(""Day"");");
-
-            var builder = new SqlQueryBuilder(null);
-            IRegardQuery result = builder.AllEvents();
-            result = result.Only("EventType", "DoSomething").CountUniqueValues("SessionId", "NumSessions").BrokenDownBy("Day", "Day");
-
-            Console.WriteLine("");
-            Console.WriteLine(((SqlQuery)result).GenerateQuery());
-            Console.WriteLine("");
-            Console.WriteLine("Substitutions:");
-            foreach (var sub in ((SqlQuery) result).GenerateSubstitutions())
+            // We need to use asynchronous stuff and there's no way to make an entry point async in C# (though there really should be), so run as a task
+            var runIt = Task.Run(async () => 
             {
-                Console.WriteLine("  {0}", sub);
-            }
+                Console.WriteLine(@"builder.AllEvents().Only(""EventType"", ""DoSomething"").CountUniqueValues(""SessionId"").BrokenDownBy(""Day"");");
 
-            Console.ReadLine();
+                var connection = new SqlConnection("");
+                var builder = new SqlQueryBuilder(connection);
+                IRegardQuery result = builder.AllEvents();
+                result = result.Only("EventType", "DoSomething").CountUniqueValues("SessionId", "NumSessions").BrokenDownBy("Day", "Day");
+
+                Console.WriteLine("");
+                Console.WriteLine(((SqlQuery)result).GenerateQuery());
+                Console.WriteLine("");
+                Console.WriteLine("Substitutions:");
+                foreach (var sub in ((SqlQuery) result).GenerateSubstitutions())
+                {
+                    Console.WriteLine("  {0} = {1}", sub.Name, sub.Value);
+                }
+
+                Console.WriteLine("Press enter to run the query...");
+                Console.ReadLine();
+
+                await connection.OpenAsync();
+                var queryResult = await result.RunQuery();
+
+                for (var nextLine = await queryResult.FetchNext(); nextLine != null; nextLine = await queryResult.FetchNext())
+                {
+                    Console.WriteLine("=== NEW LINE: {0} events", nextLine.EventCount);
+
+                    foreach (var column in nextLine.Columns)
+                    {
+                        Console.WriteLine("  {0} = {1}", column.Name, column.Value);
+                    }
+                }
+            });
+
+            runIt.Wait();
         }
     }
 }
