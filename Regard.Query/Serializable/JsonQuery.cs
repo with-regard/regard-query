c304caf1b4dd2942d13fecdc9eb51f91fe34d290
Andrew Hunter <andrew.hunter@red-gate.com>
2014-03-31 17:06:18 +0100

    Add a way to create a JObject for a query

---
+++ Regard.Query/Serializable/JsonQuery.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Regard.Query.Serializable
+{
+    /// <summary>
+    /// Utility method for creating/decoding querys using JSON
+    /// </summary>
+    public static class JsonQuery
+    {
+        /// <summary>
+        /// Converts a serializable query into a JObject that can be used for storage or transmission
+        /// </summary>
+        public static JObject ToJson(this SerializableQuery query)
+        {
+            // Just create an empty object for the null query
+            if (query == null)
+            {
+                return new JObject();
+            }
+            
+            // Recursively build up the query
+            JObject appliesTo = new JObject();
+            if (query.AppliesTo != null)
+            {
+                appliesTo = query.ToJson();
+            }
+
+            // Create the result object
+            JObject result = new JObject();
+
+            result["verb"]          = query.Verb;
+            result["applies-to"]    = appliesTo;
+
+            if (query.Key != null)      result["key"]       = query.Key;
+            if (query.Value != null)    result["value"]     = query.Value;
+            if (query.Name != null)     result["name"]      = query.Name;
+
+            return result;
+        }
+    }
+}
