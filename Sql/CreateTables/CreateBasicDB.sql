6917089522c52fd31573e55cd392671710b09d3b
Andrew Hunter <andrew.hunter@red-gate.com>
2014-03-31 11:36:11 +0100

    Add tables to record individual sessions and opted-in users

--- Sql/CreateTables/CreateBasicDB.sql
+++ Sql/CreateTables/CreateBasicDB.sql
@@ -10,48 +10,54 @@ GO
 --
 -- Delete the existing tables (danger!)
 --
-DROP TABLE Product;
-DROP TABLE Event;
-DROP TABLE EventProperty;
-DROP TABLE EventPropertyValues;
+DROP TABLE [Product];
+DROP TABLE [Event];
+DROP TABLE [EventProperty];
+DROP TABLE [EventPropertyValues];
+DROP TABLE [OptInUser]
+DROP TABLE [Session]
 
 GO
 
 --
 -- Represents an individual product that can generate events
 --
-CREATE TABLE Product
+CREATE TABLE [Product]
 	(
-		[Id] int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+		[Id] bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
 		[Name] NVARCHAR(256) NOT NULL,
 		[Organization] NVARCHAR(256) NOT NULL
 	)
+	;
 
 --
 -- The Event table assigns IDs to events
 --
-CREATE TABLE Event
+CREATE TABLE [Event]
 	(
 		[Id] bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
-		[ProductId] int NOT NULL
-		
+		[ProductId] bigint NOT NULL,
+		[ShortSessionId] bigint NOT NULL
+				
 		-- TODO: product version?
 		-- TODO: session identifier?
 	)
+	;
 
 --
 -- The EventProperty table gives IDs to property names
 --
-CREATE TABLE EventProperty
+CREATE TABLE [EventProperty]
 	(
 		[Id] int NOT NULL IDENTITY(1,1),
 		[Name] NVARCHAR(256) NOT NULL PRIMARY KEY
 	)
+	;
 	
 --
 -- The Event property values table maps 
 --
-CREATE TABLE EventPropertyValues
+CREATE TABLE [EventPropertyValues]
 	(
 		[EventId] int NOT NULL,
 		-- [PropertyId] int NOT NULL,
@@ -60,5 +66,31 @@ CREATE TABLE EventPropertyValues
 		
 		PRIMARY KEY ([EventId], [PropertyName])
 	)
+	;
+
+--
+-- The users table identifies opted-in users
+--
+-- Events MUST NOT be recorded for users who are not in this table.
+--
+CREATE TABLE [OptInUser]
+	(
+		[FullUserID] uniqueidentifier PRIMARY KEY,						-- How the user identifies to us
+		[ShortUserID] bigint NOT NULL IDENTITY(1,1)						-- How the user is identified within the database
+	)
+	;
+
+--
+-- The session table indicate runs through the application
+--
+-- Sessions MUST NOT be recorded for users who are not in the opt in table
+--
+CREATE TABLE [Session]
+	(
+		[FullSessionID] uniqueidentifier PRIMARY KEY,					-- How the user's app identifies the session to us
+		[ShortSessionID] bigint NOT NULL,								-- How the session is identified within the database
+		[ShortUserID] bigint NOT NULL									-- Identifies the user that the session is for
+	)
+	;
 
 GO
