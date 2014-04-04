bd292d65e19cd5ba6748ba5476317220fdf21611
Andrew Hunter <andrew.hunter@red-gate.com>
2014-04-04 13:15:30 +0100

    Generate events estimated to be equivalent to a medium-sized OS project with around 10000 active users.

--- Regard.Query.Samples/Run.cs
+++ Regard.Query.Samples/Run.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using Regard.Query.Api;
 using Regard.Query.Sql;
 
@@ -8,6 +9,62 @@ namespace Regard.Query.Samples
 {
     public static class Run
     {
+        /// <summary>
+        /// Auto-generates data for load testing
+        /// </summary>
+        public static async Task GenerateData(IEventRecorder recorder, Guid userId, int numSessions, int numEvents, int randomSeed)
+        {
+            Random rng = new Random(randomSeed);
+
+            // Repetition makes certain events more likely
+            string[] eventTypes = new[] {"Click", "Click", "Click", "Click", "Drag", "Drag", "Rock", "Roll"};
+
+            DateTime lastTick = DateTime.Now;
+            DateTime start = DateTime.Now;
+            int eventsSinceLastTick = 0;
+
+            for (int session = 0; session < numSessions; ++session)
+            {
+                var sessionId = await recorder.StartSession("WithRegard", "Test", userId);
+
+                var day = rng.Next(256);
+
+                await recorder.RecordEvent(sessionId,
+                    JObject.FromObject(new
+                    {
+                        Day         = day,
+                        SessionId   = session,
+                        EventType   = "BeginSession"
+                    }));
+
+                ++eventsSinceLastTick;
+
+                // TODO: different numbers of events for different sessions?
+                for (int evt = 1; evt < numEvents; ++evt)
+                {
+                    await recorder.RecordEvent(sessionId, JObject.FromObject(new
+                        {
+                            Day         = day,
+                            SessionId   = session,
+                            EventType   = eventTypes[rng.Next(eventTypes.Length)]
+                        }));
+
+                    DateTime now = DateTime.Now;
+                    if ((now - lastTick) > TimeSpan.FromSeconds(1))
+                    {
+                        Console.WriteLine("{0}/{1} sessions ({2} events per second)", session, numSessions, ((double)eventsSinceLastTick) / (now - lastTick).TotalSeconds);
+
+                        lastTick = now;
+                        eventsSinceLastTick = 0;
+                    }
+
+                    ++eventsSinceLastTick;
+                }
+            }
+
+            Console.WriteLine("Total: {0} seconds", (DateTime.Now - start).TotalSeconds);
+        }
+
         [STAThread]
         public static void Main()
         {
@@ -16,10 +73,36 @@ namespace Regard.Query.Samples
             {
                 Console.WriteLine(@"builder.AllEvents().Only(""EventType"", ""DoSomething"").CountUniqueValues(""SessionId"").BrokenDownBy(""Day"");");
 
+                // Create the database connection
                 var connection = new SqlConnection("");
+                await connection.OpenAsync();
+
+                // Generate some data
+                var recorder = new SqlEventRecorder(connection);
+                var sessionId = await recorder.StartSession("WithRegard", "Test", WellKnownUserIdentifier.TestUser);
+
+                await recorder.RecordEvent(sessionId, JObject.FromObject(new
+                    {
+                        Day = 1,
+                        Test = "Hello"
+                    }));
+
+                await recorder.RecordEvent(sessionId, JObject.FromObject(new
+                {
+                    Day = 203,
+                    EventType = "DoSomething",
+                    SessionId = 8
+                }));
+
+                // 10000 sessions of 100 events each.
+                // 10000 sessions is likely from a medium-sized open-source project
+                // 100 events per session is on the high side but not necessarily unreasonable
+                await GenerateData(recorder, WellKnownUserIdentifier.TestUser, 10000, 100, 1);
+
+                // Try querying the database
                 var builder = new SqlQueryBuilder(connection, 1, WellKnownUserIdentifier.ProductDeveloper);
                 IRegardQuery result = builder.AllEvents();
-                result = result.Only("EventType", "DoSomething").CountUniqueValues("SessionId", "NumSessions").BrokenDownBy("Day", "Day");
+                result = result.Only("EventType", "Click").CountUniqueValues("SessionId", "NumSessions").BrokenDownBy("Day", "Day");
 
                 Console.WriteLine("");
                 Console.WriteLine(((SqlQuery)result).GenerateQuery());
@@ -33,7 +116,6 @@ namespace Regard.Query.Samples
                 Console.WriteLine("Press enter to run the query...");
                 Console.ReadLine();
 
-                await connection.OpenAsync();
                 var queryResult = await result.RunQuery();
 
                 for (var nextLine = await queryResult.FetchNext(); nextLine != null; nextLine = await queryResult.FetchNext())
