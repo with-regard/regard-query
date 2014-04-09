bd0eb4ce60d116a302ab637b175a178093a15d8d
Andrew Hunter <andrew.hunter@red-gate.com>
2014-04-09 14:30:40 +0100

    Downgrade the storage client to 2.1.0.0 as the newer version doesn't seem to work too well. Initial work on a test of the performance of the flat table store to see if it's worth pursuing.

--- Regard.Query.Samples/Run.cs
+++ Regard.Query.Samples/Run.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 using Newtonsoft.Json.Linq;
 using Regard.Query.Api;
+using Regard.Query.Flat;
 using Regard.Query.Sql;
 
 namespace Regard.Query.Samples
@@ -65,12 +68,79 @@ namespace Regard.Query.Samples
             Console.WriteLine("Total: {0} seconds", (DateTime.Now - start).TotalSeconds);
         }
 
+        /// <summary>
+        /// Attempts to write data to a table using a pipeline directly to see how many events we can process at a time
+        /// </summary>
+        private static async Task TestAzurePipeline(CloudTable targetTable, int numSessions, int numEvents, int randomSeed)
+        {
+            Random rng = new Random(randomSeed);
+
+            // Repetition makes certain events more likely
+            string[] eventTypes = new[] { "Click", "Click", "Click", "Click", "Drag", "Drag", "Rock", "Roll" };
+
+            DateTime lastTick = DateTime.Now;
+            DateTime start = DateTime.Now;
+            int eventsSinceLastTick = 0;
+
+            for (int session = 0; session < numSessions; ++session)
+            {
+                var day = rng.Next(256);
+
+                ++eventsSinceLastTick;
+
+                // TODO: different numbers of events for different sessions?
+                for (int evt = 1; evt < numEvents; ++evt)
+                {
+                    var eventPipeline = new AzureFlatPipeline(targetTable, "WithRegard", "Test", "Query1");
+
+                    var testEvent = new
+                    {
+                        Day = day,
+                        SessionId = session,
+                        EventType = eventTypes[rng.Next(eventTypes.Length)]
+                    };
+
+                    // Act like the query we'll run later
+                    if (testEvent.EventType != "Click") eventPipeline.Drop();                   // Only("EventType", "Click")
+                    eventPipeline.Bucket(testEvent.Day.ToString());                             // BrokenDownBy("Day")
+                    eventPipeline.Count("TotalEvents");
+                    eventPipeline.CountUnique("NumSessions", testEvent.SessionId.ToString());   // CountUniqueValues("SessionId", "NumSessions")
+
+                    await eventPipeline.Store();
+
+                    // Write out current performance
+                    DateTime now = DateTime.Now;
+                    if ((now - lastTick) > TimeSpan.FromSeconds(1))
+                    {
+                        Console.WriteLine("{0}/{1} sessions ({2} events per second)", session, numSessions, ((double)eventsSinceLastTick) / (now - lastTick).TotalSeconds);
+
+                        lastTick = now;
+                        eventsSinceLastTick = 0;
+                    }
+
+                    ++eventsSinceLastTick;
+                }
+            }
+
+            Console.WriteLine("Total: {0} seconds", (DateTime.Now - start).TotalSeconds);
+        }
+
         [STAThread]
         public static void Main()
         {
             // We need to use asynchronous stuff and there's no way to make an entry point async in C# (though there really should be), so run as a task
-            var runIt = Task.Run(async () => 
+            var runIt = Task.Run(async () =>
             {
+                var storageAccount = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+                var storageClient = storageAccount.CreateCloudTableClient();
+                var testTable = storageClient.GetTableReference("PerformanceTest");
+
+                await testTable.CreateIfNotExistsAsync();
+
+                await TestAzurePipeline(testTable, 10000, 100, 1);
+
+                return;
+
                 Console.WriteLine(@"builder.AllEvents().Only(""EventType"", ""DoSomething"").CountUniqueValues(""SessionId"").BrokenDownBy(""Day"");");
 
                 // Create the database connection
@@ -88,11 +158,11 @@ namespace Regard.Query.Samples
                     }));
 
                 await recorder.RecordEvent(sessionId, JObject.FromObject(new
-                {
-                    Day = 203,
-                    EventType = "DoSomething",
-                    SessionId = 8
-                }));
+                    {
+                        Day = 203,
+                        EventType = "DoSomething",
+                        SessionId = 8
+                    }));
 
                 // 10000 sessions of 100 events each.
                 // 10000 sessions is likely from a medium-sized open-source project
