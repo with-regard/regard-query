fc478364ab9aea77ba1ecd3032e4450d57800f14
Andrew Hunter <andrew.hunter@red-gate.com>
2014-04-11 17:11:24 +0100

    Start to investigate Couch as a possible data store target. Prototype event recorder.

---
+++ Regard.Query/Couch/CouchEventRecorder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Regard.Query.Api;
+
+namespace Regard.Query.Couch
+{
+    /// <summary>
+    /// Class that records events in an Apache CouchDB database
+    /// </summary>
+    public class CouchEventRecorder : IEventRecorder
+    {
+        /// <summary>
+        /// The URI of the CouchDB database that this should write events to
+        /// </summary>
+        private readonly Uri m_CouchDbUri;
+
+        public CouchEventRecorder(Uri couchDbUri)
+        {
+            if (couchDbUri == null)
+            {
+                throw new ArgumentNullException("couchDbUri");
+            }
+
+            m_CouchDbUri = couchDbUri;
+        }
+
+        /// <summary>
+        /// Indicates that a new session has begun
+        /// </summary>
+        /// <param name="organization">The name of the organization that the session is for</param>
+        /// <param name="product">The name of the product that the session is for</param>
+        /// <param name="userId">A GUID that identifies the user that this session is for</param>
+        /// <returns>A GUID that identifies this session, or Guid.Empty if the session can't be started (because the user is opted-out, for example)</returns>
+        public async Task<Guid> StartSession(string organization, string product, Guid userId)
+        {
+            // Create a GUID for this session
+            var sessionGuid = Guid.NewGuid();
+
+            // Create a new session document
+            JObject sessionDocument = new JObject();
+
+            sessionDocument["session-id"]   = sessionGuid.ToString();
+            sessionDocument["organization"] = organization;
+            sessionDocument["product"]      = product;
+
+            await CouchUtil.PutDocuments(m_CouchDbUri, "sessions", new[] { new KeyValuePair<string, JObject>("session/" + sessionGuid, sessionDocument) });
+
+            return sessionGuid;
+        }
+
+        /// <summary>
+        /// Schedules a single event to be recorded by this object
+        /// </summary>
+        /// <param name="sessionId">The ID of the session (as returned by StartSession)</param>
+        /// <param name="data">JSON data indicating the properties for this event</param>
+        public async Task RecordEvent(Guid sessionId, JObject data)
+        {
+            // TODO: retrieve the database that corresponds to this session ID
+            string database = "temp-testdb";
+
+            // Store data for this event
+            Guid    eventGuid   = Guid.NewGuid();
+            JObject eventObject = new JObject();
+
+            eventObject["session-id"]   = sessionId.ToString();
+            eventObject["user-data"]    = data;
+
+            await CouchUtil.PutDocuments(m_CouchDbUri, database, new[] { new KeyValuePair<string, JObject>("events/" + sessionId + "/" + eventGuid, eventObject) });
+        }
+    }
+}
---
+++ Regard.Query/Couch/CouchUtil.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Regard.Query.Couch
+{
+    /// <summary>
+    /// Utility classes for helping out with Couch
+    /// </summary>
+    public static class CouchUtil
+    {
+        private const string c_InvalidChars = @"([\/\\\#\?\-]|[\x00-\x1f]|[\x7f-\x9f])";
+
+        /// <summary>
+        /// Converts a string to one that contains characters that can be encoded within a single URI path component
+        /// </summary>
+        public static string SanitiseAsUriComponent(string value)
+        {
+            if (value == null) return null;
+
+            return Regex.Replace(value, c_InvalidChars, match =>
+            {
+                StringBuilder result = new StringBuilder();
+
+                result.Append('-');
+                foreach (var matchChar in match.Value)
+                {
+                    result.Append(((int)matchChar).ToString("x"));
+                }
+                result.Append('-');
+
+                return result.ToString();
+            });
+        }
+
+        /// <summary>
+        /// Retrieves the name of the database to use for a specific organization/product
+        /// </summary>
+        public static string GetDatabaseName(string organization, string product)
+        {
+            return SanitiseAsUriComponent(organization + '/' + product);
+        }
+
+        /// <summary>
+        /// Encodes a JObject as UTF-8 bytes
+        /// </summary>
+        private static byte[] ToBytesUTF8(JObject obj)
+        {
+            return Encoding.UTF8.GetBytes(obj.ToString(Formatting.None));
+        }
+
+        /// <summary>
+        /// Inserts a set of documents into a CouchDB database
+        /// </summary>
+        public static async Task<WebResponse> PutDocuments(Uri couchDbUri, string database, IEnumerable<KeyValuePair<string, JObject>> documents)
+        {
+            if (documents == null)
+            {
+                throw new ArgumentNullException("documents");
+            }
+
+            // Work out the location of the bulk docs URI for this database
+            var databaseUri = new Uri(couchDbUri, database);
+            var bulkUri     = new Uri(databaseUri, "_bulk_docs");
+
+            // Generate the request object
+            JObject requestObject   = new JObject();
+            JArray  docs            = new JArray();
+
+            foreach (var doc in documents)
+            {
+                JObject newDoc  = new JObject(doc.Value);
+                newDoc["_id"]   = doc.Key;
+
+                docs.Add(newDoc);
+            }
+
+            requestObject["docs"] = docs;
+
+            // Convert to a byte array
+            var requestBytes = ToBytesUTF8(requestObject);
+            
+            // Start putting together a request
+            var request = WebRequest.Create(bulkUri);
+
+            request.Method          = "PUT";
+            request.ContentType     = "application/json";
+            request.ContentLength   = requestBytes.Length;
+
+            using (var stream = await request.GetRequestStreamAsync())
+            {
+                await stream.WriteAsync(requestBytes, 0, requestBytes.Length);
+                stream.Close();
+            }
+
+            // Perform the update
+            return await request.GetResponseAsync();
+        }
+    }
+}
