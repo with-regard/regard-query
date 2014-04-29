6cd62e4961c25e216b10992b5da96480469a554b
Andrew Hunter <andrew.hunter@red-gate.com>
2014-04-29 14:26:04 +0100

    Ugh, Couch is so close to what we want, except for the JavaScript, file size, lack of M/R chaining and other problems.
    Looking in to a custom M/R system based on flat tables (WACEL already has one but it's horrible): define the interfaces that we'll need.

---
+++ Regard.Query/Api/IMapReduce.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace Regard.Query.Api
+{
+    /// <summary>
+    /// Interface that can be implemented by objects that can perform a map/reduce query
+    /// </summary>
+    public interface IMapReduce
+    {
+        /// <summary>
+        /// Maps a document onto a target
+        /// </summary>
+        /// <param name="target">The target where the mapped documents should be emitted</param>
+        /// <param name="document">The document that is being mapped</param>
+        void Map(IMapTarget target, JObject document);
+
+        /// <summary>
+        /// Reduces a set of documents with the same key into a single output document
+        /// </summary>
+        /// <param name="key">The name of the key assigned to these documents during the Map operation</param>
+        /// <param name="documents">The documents to reduce</param>
+        /// <returns>An object representing the result of reducing these documents to the final value for this key</returns>
+        JObject Reduce(string key, IEnumerable<JObject> documents);
+
+        /// <summary>
+        /// Updates the result of a reduce operation (combines the result of several reductions)
+        /// </summary>
+        /// <param name="key">The name of the key assigned to the document being reduced</param>
+        /// <param name="reductions">The reductions to combine</param>
+        /// <returns>An object representing the combined reduction</returns>
+        JObject Rereduce(string key, IEnumerable<JObject> reductions);
+    }
+}
