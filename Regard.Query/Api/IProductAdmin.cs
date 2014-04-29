4ce74afa62587589c7c7c2c309ce1673c0eaf7ae
Andrew Hunter <andrew.hunter@red-gate.com>
2014-04-29 16:54:59 +0100

    Create an interface representing a data store and the methods to create/retrieve a product

---
+++ Regard.Query/Api/IProductAdmin.cs
@@ -0,0 +1,18 @@
+﻿namespace Regard.Query.Api
+{
+    /// <summary>
+    /// Retrieves and creates products that can have events recorded against them
+    /// </summary>
+    public interface IProductAdmin
+    {
+        /// <summary>
+        /// Creates a new product that can have events logged against it
+        /// </summary>
+        void CreateProduct(string organization, string product);
+
+        /// <summary>
+        /// For a product that exists, retrieves the interface for interacting with its queries
+        /// </summary>
+        IQueryableProduct GetProduct(string organization, string product);
+    }
+}
