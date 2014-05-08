08b09b7ec409087c26061bc7a9525be68a0a4923
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-08 15:29:45 +0100

    Add two new tests for CountUnique, dealing with the effects of re-reduce

--- Regard.Query.Tests/MapReduce/CountUnique.cs
+++ Regard.Query.Tests/MapReduce/CountUnique.cs
@@ -18,8 +18,8 @@ namespace Regard.Query.Tests.MapReduce
             {
                 // Create the 'only clicks' query
                 var queryBuilder = new SerializableQueryBuilder(null);
-                var onlyClicks = (SerializableQuery)queryBuilder.AllEvents().CountUniqueValues("SessionId", "NumSessions");
-                var query = onlyClicks.GenerateMapReduce();
+                var uniqueSessions = (SerializableQuery)queryBuilder.AllEvents().CountUniqueValues("SessionId", "NumSessions");
+                var query = uniqueSessions.GenerateMapReduce();
 
                 // Generate a data store and an ingestor
                 var resultStore = new MemoryKeyValueStore();
@@ -40,6 +40,90 @@ namespace Regard.Query.Tests.MapReduce
 
                     // There are 12 total events
                     Assert.AreEqual(12, nextRecord.Item2["Count"].Value<int>());
+                    recordCount++;
+                }
+
+                // Should be only one record
+                Assert.AreEqual(1, recordCount);
+            });
+
+            task.Wait();
+        }
+
+        [Test]
+        public void UniqueSessionsRereduce()
+        {
+            var task = Task.Run(async () =>
+            {
+                // Create the 'unique sessions' query
+                var queryBuilder = new SerializableQueryBuilder(null);
+                var uniqueSessions = (SerializableQuery)queryBuilder.AllEvents().CountUniqueValues("SessionId", "NumSessions");
+                var query = uniqueSessions.GenerateMapReduce();
+
+                // Generate a data store and an ingestor
+                var resultStore = new MemoryKeyValueStore();
+                var ingestor = new DataIngestor(query, resultStore);
+
+                // Run the standard set of docs through twice
+                await Util.TestBasicDocuments(ingestor);
+                await Util.TestBasicDocuments(ingestor);
+
+                // This should create a data store with one record indicating that there are 12 records 
+                var reader = resultStore.EnumerateAllValues();
+                int recordCount = 0;
+
+                Tuple<JArray, JObject> nextRecord;
+                while ((nextRecord = await reader.FetchNext()) != null)
+                {
+                    // There are 3 unique sessions even when re-reducing
+                    Assert.AreEqual(3, nextRecord.Item2["NumSessions"].Value<int>());
+
+                    // There are 24 total events
+                    Assert.AreEqual(24, nextRecord.Item2["Count"].Value<int>());
+                    recordCount++;
+                }
+
+                // Should be only one record
+                Assert.AreEqual(1, recordCount);
+            });
+
+            task.Wait();
+        }
+
+        [Test]
+        public void UniqueSessionsRereduceNew()
+        {
+            var task = Task.Run(async () =>
+            {
+                // Create the 'unique sessions' query
+                var queryBuilder = new SerializableQueryBuilder(null);
+                var uniqueSessions = (SerializableQuery)queryBuilder.AllEvents().CountUniqueValues("SessionId", "NumSessions");
+                var query = uniqueSessions.GenerateMapReduce();
+
+                // Generate a data store and an ingestor
+                var resultStore = new MemoryKeyValueStore();
+                var ingestor = new DataIngestor(query, resultStore);
+
+                // Run the standard set of docs through twice
+                await Util.TestBasicDocuments(ingestor);
+                await Util.TestBasicDocuments(ingestor);
+
+                // Add a new session too
+                ingestor.Ingest(JObject.FromObject(new { SessionId = "4" }));
+                await ingestor.Commit();
+
+                // This should create a data store with one record indicating that there are 12 records 
+                var reader = resultStore.EnumerateAllValues();
+                int recordCount = 0;
+
+                Tuple<JArray, JObject> nextRecord;
+                while ((nextRecord = await reader.FetchNext()) != null)
+                {
+                    // There are 3 unique sessions even when re-reducing
+                    Assert.AreEqual(4, nextRecord.Item2["NumSessions"].Value<int>());
+
+                    // There are 24 total events
+                    Assert.AreEqual(25, nextRecord.Item2["Count"].Value<int>());
                     recordCount++;
                 }
 
