614933507d0faf2f16d5750a8e8cbbfea61d4b92
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-08 14:54:01 +0100

    Add a test for the re-reduce operation

--- Regard.Query.Tests/MapReduce/AllEvents.cs
+++ Regard.Query.Tests/MapReduce/AllEvents.cs
@@ -47,5 +47,42 @@ namespace Regard.Query.Tests.MapReduce
 
             task.Wait();
         }
+
+        [Test]
+        public void Rereduce()
+        {
+            var task = Task.Run(async () =>
+            {
+                // Create the 'all events query'
+                var queryBuilder = new SerializableQueryBuilder(null);
+                var allEvents = queryBuilder.AllEvents();
+                var query = allEvents.GenerateMapReduce();
+
+                // Generate a data store and an ingestor
+                var resultStore = new MemoryKeyValueStore();
+                var ingestor = new DataIngestor(query, resultStore);
+
+                // Run the standard set of docs through twice (forcing a re-reduce)
+                await Util.TestBasicDocuments(ingestor);
+                await Util.TestBasicDocuments(ingestor);
+
+                // This should create a data store with one record indicating that there are 12 records 
+                var reader = resultStore.EnumerateAllValues();
+                int recordCount = 0;
+
+                Tuple<JArray, JObject> nextRecord;
+                while ((nextRecord = await reader.FetchNext()) != null)
+                {
+                    // Should contain a count of 24
+                    Assert.AreEqual(24, nextRecord.Item2["Count"].Value<int>());
+                    recordCount++;
+                }
+
+                // Should be only one record
+                Assert.AreEqual(1, recordCount);
+            });
+
+            task.Wait();
+        }
     }
 }
