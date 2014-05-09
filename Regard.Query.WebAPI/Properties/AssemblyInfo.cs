4d0990810328b33bd54b0aa0aa99e9df81260855
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-09 14:50:42 +0100

    Create a new project to contain a Web API interface to the query library

---
+++ Regard.Query.WebAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Regard.Query.WebAPI")]
+[assembly: AssemblyDescription("Web API for the Regard Query module")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RedGate Software")]
+[assembly: AssemblyProduct("Regard.Query.WebAPI")]
+[assembly: AssemblyCopyright("Copyright © RedGate Software 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f6632cf-d042-4729-916b-1a15babd1cdc")]
