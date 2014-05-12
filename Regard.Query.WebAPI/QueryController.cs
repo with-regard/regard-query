c85c64130d20043cbf295927d104c9287b4df9c9
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-12 09:40:00 +0100

    Implement the query registration request

--- Regard.Query.WebAPI/QueryController.cs
+++ Regard.Query.WebAPI/QueryController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -7,6 +8,7 @@ using System.Web.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Regard.Query.Api;
+using Regard.Query.Serializable;
 
 namespace Regard.Query.WebAPI
 {
@@ -40,13 +42,6 @@ namespace Regard.Query.WebAPI
         [HttpPost, Route("admin/v1/product/create")]
         public async Task<HttpResponseMessage> CreateProduct()
         {
-            // The default response is 'bad request'
-            var response = new HttpResponseMessage();
-            response.StatusCode = HttpStatusCode.BadRequest;
-
-            // The content will be JSON
-            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
             // Read the payload from the message
             var payloadString = await Request.Content.ReadAsStringAsync();
 
@@ -97,26 +92,118 @@ namespace Regard.Query.WebAPI
         /// <summary>
         /// Registers a query for a particular product
         /// </summary>
-        [HttpPost, Route("product/v1/{product}/{organization}/register-query")]
-        public async Task<HttpResponseMessage> RegisterQuery()
+        /// <remarks>
+        /// Queries must be registered before they can be used; this supports map/reduce style databases where ad-hoc queries are
+        /// very slow, as well as allowing for a future update to support real-time updating.
+        /// <para/>
+        /// Accepts a 
+        /// </remarks>
+        [HttpPost, Route("product/v1/{organization}/{product}/register-query")]
+        public async Task<HttpResponseMessage> RegisterQuery(string organization, string product)
         {
-            throw new NotImplementedException();
+            // Check that the org/product exsits
+            var queryableProduct = await m_DataStore.Products.GetProduct(organization, product);
+            if (queryableProduct == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Could not find product");
+            }
+
+            // Read the payload from the message
+            var payloadString = await Request.Content.ReadAsStringAsync();
+
+            // Convert to JSON
+            JObject payload;
+            try
+            {
+                payload = JObject.Parse(payloadString);
+            }
+            catch (JsonException)
+            {
+                // This is a bad request
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Could not understand payload");
+            }
+
+            // Should be a name and a query
+            JToken nameToken;
+            JToken queryToken;
+            JObject query;
+
+            if (!payload.TryGetValue("query-name", out nameToken))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Missing query name");
+            }
+
+            if (!payload.TryGetValue("query-definition", out queryToken))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Missing query definition");
+            }
+
+            query = queryToken as JObject;
+            if (query == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid query definition");
+            }
+
+            // Validate the name
+            string name;
+            if (nameToken.Type != JTokenType.String)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Name must be a string");
+            }
+
+            name = nameToken.Value<string>();
+            if (string.IsNullOrEmpty(name) || name.Length > 200)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Name has a bad length");
+            }
+
+            // Try to deserialize the query
+            var queryBuilder = queryableProduct.CreateQueryBuilder();
+            if (queryBuilder == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Query builder missing");
+            }
+
+            IRegardQuery builtQuery;
+
+            // Will throw an InvalidOperationException if the query is bad
+            try
+            {
+                builtQuery = queryBuilder.FromJson(query);
+                if (builtQuery == null)
+                {
+                    // Shouldn't happen, I think
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Query failed to build");
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Trace.WriteLine("QueryController: could not build query: " + e);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid query definition");
+            }
+
+            // Register the query
+            await queryableProduct.RegisterQuery(name, builtQuery);
+
+            // OK
+            return Request.CreateResponse(HttpStatusCode.Created, new {});
         }
 
         /// <summary>
-        /// Runs a query previously created by register-query
+        /// Runs a query previously created by register-query and returns all of the results
         /// </summary>
-        [HttpGet, Route("product/v1/{product}/{organization}/run-query/{queryname}")]
-        public async Task<HttpResponseMessage> RunQuery()
+        [HttpGet, Route("product/v1/{organization}/{product}/run-query/{queryname}")]
+        public async Task<HttpResponseMessage> RunQuery(string organization, string product)
         {
+            // TODO: if a query produces a very large number of results, this will be inefficient
             throw new NotImplementedException();
         }
 
         /// <summary>
         /// Opts in a particular user
         /// </summary>
-        [HttpGet, Route("product/v1/{product}/{organization}/users/{uid}/opt-in")]
-        public async Task<HttpResponseMessage> OptIn()
+        [HttpGet, Route("product/v1/{organization}/{product}/users/{uid}/opt-in")]
+        public async Task<HttpResponseMessage> OptIn(string organization, string product)
         {
             throw new NotImplementedException();
         }
@@ -128,8 +215,8 @@ namespace Regard.Query.WebAPI
         /// That is, moves the user to the state 'data is retained but not used in a query'. A future revision will have 'data is also deleted'.
         /// It's expected that the application will not send data for an opted out user. If it does, new data will be stored but won't be used in a query.
         /// </remarks>
-        [HttpGet, Route("product/v1/{product}/{organization}/users/{uid}/opt-out")]
-        public async Task<HttpResponseMessage> OptOut()
+        [HttpGet, Route("product/v1/{organization}/{product}/users/{uid}/opt-out")]
+        public async Task<HttpResponseMessage> OptOut(string organization, string product)
         {
             throw new NotImplementedException();
         }
