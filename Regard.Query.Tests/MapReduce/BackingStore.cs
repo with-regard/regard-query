8ebdc2767b676b054da00428793f32f29b759c34
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-19 16:27:01 +0100

    Hm, we'll need some tests for the backing store to make sure it performs correctly

---
+++ Regard.Query.Tests/MapReduce/BackingStore.cs
@@ -0,0 +1,111 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Regard.Query.Api;
+using Regard.Query.MapReduce;
+
+namespace Regard.Query.Tests.MapReduce
+{
+    [TestFixture("InMemory")]
+    class BackingStore
+    {
+        private string m_DataStoreType;
+
+        public BackingStore(string storeType)
+        {
+            m_DataStoreType = storeType;
+        }
+
+        /// <summary>
+        /// Creates a backing store store of the given type
+        /// </summary>
+        private IKeyValueStore CreateStoreToTest()
+        {
+            switch (m_DataStoreType)
+            {
+                case "InMemory": return new MemoryKeyValueStore();
+                default:
+                    Assert.Fail();
+                    return null;
+            }
+        }
+
+        [Test]
+        public void CanCreateAnEmptyStore()
+        {
+            CreateStoreToTest();
+        }
+
+        [Test]
+        public void KeysDefaultToNull()
+        {
+            Task.Run(async () =>
+            {
+                var store = CreateStoreToTest();
+
+                var value = await store.GetValue(JArray.FromObject(new[] { "test-key" }));
+                Assert.IsNull(value);
+            }).Wait();
+        }
+
+        [Test]
+        public void CanStoreAndRetrieveADocument()
+        {
+            Task.Run(async () =>
+            {
+                var store = CreateStoreToTest();
+
+                var key = JArray.FromObject(new[] { "test-key" });
+                var storeValue = JObject.FromObject(new {SomeValue = "hello" });
+
+                await store.SetValue(key, storeValue);
+                var value = await store.GetValue(key);
+
+                Assert.IsNotNull(value);
+                Assert.AreEqual("hello", value["SomeValue"].Value<string>());
+            }).Wait();
+        }
+
+        [Test]
+        public void CanRetrieveAChildStore()
+        {
+            Task.Run(async () =>
+            {
+                var store = CreateStoreToTest();
+
+                var key = JArray.FromObject(new[] { "test-key" });
+
+                var child = store.ChildStore(key);
+
+                Assert.IsNotNull(child);
+            }).Wait();
+        }
+
+        [Test]
+        public void ValuesAreDifferentInChildFromParent()
+        {
+            Task.Run(async () =>
+            {
+                var store = CreateStoreToTest();
+
+                var childStoreKey = JArray.FromObject(new[] { "test-store-key" });
+                var documentKey = JArray.FromObject(new[] { "test-key" });
+
+                var child = store.ChildStore(childStoreKey);
+                Assert.IsFalse(ReferenceEquals(child, store));
+
+                await store.SetValue(documentKey, JObject.FromObject(new { SomeValue = "MainStore" }));
+                await child.SetValue(documentKey, JObject.FromObject(new { SomeValue = "ChildStore" }));
+
+                var mainValue = await store.GetValue(documentKey);
+                var childValue = await child.GetValue(documentKey);
+
+                Assert.IsNotNull(mainValue);
+                Assert.IsNotNull(childValue);
+
+                Assert.AreEqual("MainStore", mainValue["SomeValue"].Value<string>());
+                Assert.AreEqual("ChildStore", childValue["SomeValue"].Value<string>());
+            }).Wait();
+        }
+    }
+}
