c7f58017f04ac7951ff8dd3f2ded831d572d2064
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-22 15:15:14 +0100

    Add a test for the Sum operation

---
+++ Regard.Query.Tests/MapReduce/RunMapReduce.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Regard.Query.Api;
+using Regard.Query.MapReduce;
+using Regard.Query.Serializable;
+
+namespace Regard.Query.Tests.MapReduce
+{
+    static class RunMapReduce
+    {
+        /// <summary>
+        /// Runs a map/reduce task on the basic documents
+        /// </summary>
+        public static async Task<IKeyValueStore> RunOnBasicDocuments(SerializableQuery query)
+        {
+            var mapReduce = query.GenerateMapReduce();
+            var resultStore = new MemoryKeyValueStore();
+
+            // Generate a data store and an ingestor
+            var ingestor = new DataIngestor(mapReduce, resultStore);
+
+            // Run the standard set of docs through
+            // As there are no documents in the data store currently, this will reduce but not re-reduce
+            await TestDataGenerator.Ingest12BasicDocuments(ingestor);
+
+            return resultStore;
+        }
+    }
+}
---
+++ Regard.Query.Tests/MapReduce/Sum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Regard.Query.Api;
+using Regard.Query.Serializable;
+
+namespace Regard.Query.Tests.MapReduce
+{
+    [TestFixture]
+    class Sum
+    {
+        [Test]
+        public void SumOfAllTheNumberValuesIs21()
+        {
+            Task.Run(async () =>
+            {
+                var queryBuilder = new SerializableQueryBuilder(null);
+                var results = await RunMapReduce.RunOnBasicDocuments((SerializableQuery) queryBuilder.AllEvents().Sum("NumberValue", "SumOfAllTheNumberValue"));
+
+                // There should be one number, and it should be 1+2+...+5+6 = 21
+                var reader = results.EnumerateAllValues();
+                int recordCount = 0;
+
+                Tuple<JArray, JObject> nextRecord;
+                while ((nextRecord = await reader.FetchNext()) != null)
+                {
+                    // There are 5 click events
+                    Assert.AreEqual(21, nextRecord.Item2["SumOfAllTheNumberValue"].Value<double>());
+                    recordCount++;
+                }
+
+                // Should be only one record
+                Assert.AreEqual(1, recordCount);
+            }).Wait();
+        }
+    }
+}
