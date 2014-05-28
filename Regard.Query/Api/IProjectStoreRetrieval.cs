8dace8c87d016f2376dab5e72048015eb1599dc5
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-28 14:17:34 +0100

    Going to update the data store to work on a one table per project basis to pre-empt some likely administrative problems with the one table for all approach we have at the moment. Here's the interface we'll need to implement this feature.

---
+++ Regard.Query/Api/IProjectStoreRetrieval.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Regard.Query.Api
+{
+    /// <summary>
+    /// Interface implemented by objects that retrieves Key/Value store on a per-project basis
+    /// </summary>
+    public interface IProjectStoreRetrieval
+    {
+        /// <summary>
+        /// Retrieves or creates a key/value store for a project
+        /// </summary>
+        Task<IKeyValueStore> GetStoreForProject(string organization, string project);
+    }
+}
