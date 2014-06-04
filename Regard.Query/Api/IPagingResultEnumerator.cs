48af94de93975f99e27c7ba65f26c4141164207f
Andrew Hunter <andrew.hunter@red-gate.com>
2014-06-04 17:42:21 +0100

    Add a paging results enumerator interface so we can break up things like user events into pages.

---
+++ Regard.Query/Api/IPagingResultEnumerator.cs
@@ -0,0 +1,14 @@
+﻿namespace Regard.Query.Api
+{
+    /// <summary>
+    /// Result enumerator that can be
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IPagingResultEnumerator<T> : IResultEnumerator<T> where T : class
+    {
+        /// <summary>
+        /// Fast-forwards this enumerator so that the next call to FetchNext will retrieve the element at the specified index (a positive offset from the current index)
+        /// </summary>
+        void FastForward(int index);
+    }
+}
