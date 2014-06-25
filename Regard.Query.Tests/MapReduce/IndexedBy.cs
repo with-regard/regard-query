0731760d293bf304d8dcb2fd59aa3581be431c74
Andrew Hunter <andrew.hunter@red-gate.com>
2014-06-25 15:00:28 +0100

    Add a test for unreducing when IndexedBy is used (should be able to delete documents)

--- Regard.Query.Tests/MapReduce/IndexedBy.cs
+++ Regard.Query.Tests/MapReduce/IndexedBy.cs
@@ -70,5 +70,45 @@ namespace Regard.Query.Tests.MapReduce
 
             task.Wait();
         }
+
+        [Test]
+        public void UnreductionResultsInNoDocuments()
+        {
+            // As for BrokenDownBy, but indexing by event type as well
+            // ... but also 4 in the second, and 5 in the third. THis makes the method name rather long, though.
+            // Not actually sure if these should be separate tests. I think not really as they are different failure conditions rather than different behaviours
+            var task = Task.Run(async () =>
+            {
+                // Create the 'only clicks' query
+                var queryBuilder = new SerializableQueryBuilder(null);
+                var brokenDownByDay = (SerializableQuery)queryBuilder.AllEvents().BrokenDownBy("SessionId", "WhichSession").IndexedBy("EventType", "event-type");
+                var query = brokenDownByDay.GenerateMapReduce();
+
+                // Generate a data store and an ingestor
+                var resultStore = new MemoryKeyValueStore();
+                var ingestor = new DataIngestor(query, resultStore);
+
+                // Run the standard set of docs through, then delete them
+                await TestDataGenerator.Ingest12BasicDocuments(ingestor);
+                await TestDataGenerator.Uningest12BasicDocuments(ingestor);
+
+                // This should leave an empty data store
+                var reader = resultStore.EnumerateAllValues();
+                int recordCount = 0;
+
+                Tuple<JArray, JObject> nextRecord;
+                while ((nextRecord = await reader.FetchNext()) != null)
+                {
+                    Assert.AreEqual(0, nextRecord.Item2["Count"].Value<int>());
+
+                    recordCount++;
+                }
+
+                // Should be three records, one for each session
+                Assert.AreEqual(0, recordCount);
+            });
+
+            task.Wait();
+        }
     }
 }
