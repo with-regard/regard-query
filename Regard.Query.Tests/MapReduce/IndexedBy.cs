ab2d877298e82248c844eb865562b132241c658c
Andrew Hunter <andrew.hunter@red-gate.com>
2014-06-26 15:05:58 +0100

    More tests centered around changing the order of operations when chaining

--- Regard.Query.Tests/MapReduce/IndexedBy.cs
+++ Regard.Query.Tests/MapReduce/IndexedBy.cs
@@ -135,7 +135,7 @@ namespace Regard.Query.Tests.MapReduce
         public void ThereAre2ClicksInTheSecondSessionWithDifferentOrdering()
         {
             // As for BrokenDownBy, but indexing by event type as well
-            // ... but also 4 in the second, and 5 in the third. THis makes the method name rather long, though.
+            // ... but also 4 in the second, and 5 in the third. This makes the method name rather long, though.
             // Not actually sure if these should be separate tests. I think not really as they are different failure conditions rather than different behaviours
             var task = Task.Run(async () =>
             {
@@ -202,6 +202,46 @@ namespace Regard.Query.Tests.MapReduce
                 // Create the 'only clicks' query
                 var queryBuilder = new SerializableQueryBuilder(null);
                 var brokenDownByDay = (SerializableQuery)queryBuilder.AllEvents().BrokenDownBy("SessionId", "WhichSession").IndexedBy("EventType");
+                var query = brokenDownByDay.GenerateMapReduce();
+
+                // Generate a data store and an ingestor
+                var resultStore = new MemoryKeyValueStore();
+                var ingestor = new DataIngestor(query, resultStore);
+
+                // Run the standard set of docs through, then delete them
+                await TestDataGenerator.Ingest12BasicDocuments(ingestor);
+                await TestDataGenerator.Uningest12BasicDocuments(ingestor);
+
+                // This should leave an empty data store
+                var reader = resultStore.EnumerateAllValues();
+                int recordCount = 0;
+
+                Tuple<JArray, JObject> nextRecord;
+                while ((nextRecord = await reader.FetchNext()) != null)
+                {
+                    Assert.AreEqual(0, nextRecord.Item2["Count"].Value<int>());
+
+                    recordCount++;
+                }
+
+                // Should be three records, one for each session
+                Assert.AreEqual(0, recordCount);
+            });
+
+            task.Wait();
+        }
+
+        [Test]
+        public void UnreductionResultsInNoDocumentsWhenOrderIsReversed()
+        {
+            // As for BrokenDownBy, but indexing by event type as well
+            // ... but also 4 in the second, and 5 in the third. THis makes the method name rather long, though.
+            // Not actually sure if these should be separate tests. I think not really as they are different failure conditions rather than different behaviours
+            var task = Task.Run(async () =>
+            {
+                // Create the 'only clicks' query
+                var queryBuilder = new SerializableQueryBuilder(null);
+                var brokenDownByDay = (SerializableQuery)queryBuilder.AllEvents().IndexedBy("EventType").BrokenDownBy("SessionId", "WhichSession");
                 var query = brokenDownByDay.GenerateMapReduce();
 
                 // Generate a data store and an ingestor
