6139fd4a022a7973d4e2f9dda9de5b2b5ca79667
Andrew Hunter <andrew.hunter@red-gate.com>
2014-06-26 14:36:20 +0100

    Also test IndexedBy for chain ordering behaviour differences

--- Regard.Query.Tests/MapReduce/IndexedBy.cs
+++ Regard.Query.Tests/MapReduce/IndexedBy.cs
@@ -132,6 +132,66 @@ namespace Regard.Query.Tests.MapReduce
         }
 
         [Test]
+        public void ThereAre2ClicksInTheSecondSessionWithDifferentOrdering()
+        {
+            // As for BrokenDownBy, but indexing by event type as well
+            // ... but also 4 in the second, and 5 in the third. THis makes the method name rather long, though.
+            // Not actually sure if these should be separate tests. I think not really as they are different failure conditions rather than different behaviours
+            var task = Task.Run(async () =>
+            {
+                // Create the 'only clicks' query
+                var queryBuilder = new SerializableQueryBuilder(null);
+                var brokenDownByDay = (SerializableQuery)queryBuilder.AllEvents().IndexedBy("EventType").BrokenDownBy("SessionId", "WhichSession");
+                var query = brokenDownByDay.GenerateMapReduce();
+
+                // Generate a data store and an ingestor
+                var resultStore = new MemoryKeyValueStore();
+                var ingestor = new DataIngestor(query, resultStore);
+
+                // Run the standard set of docs through
+                await TestDataGenerator.Ingest12BasicDocuments(ingestor);
+
+                // This should create a data store with one record indicating that there are 12 records 
+                var reader = resultStore.ChildStore(new JArray("chain")).EnumerateValuesBeginningWithKey(new JArray("Click"));
+                int recordCount = 0;
+
+                Tuple<JArray, JObject> nextRecord;
+                while ((nextRecord = await reader.FetchNext()) != null)
+                {
+                    switch (nextRecord.Item2["WhichSession"].Value<string>())
+                    {
+                        case "1":
+                            // There are 0 click events in the first session
+                            Assert.AreEqual(0, nextRecord.Item2["Count"].Value<int>());
+                            break;
+
+                        case "2":
+                            // 2 events in the second
+                            Assert.AreEqual(2, nextRecord.Item2["Count"].Value<int>());
+                            break;
+
+                        case "3":
+                            // 3 events in the third
+                            Assert.AreEqual(3, nextRecord.Item2["Count"].Value<int>());
+                            break;
+
+                        default:
+                            // There are sessions one, two and three
+                            Assert.Fail();
+                            break;
+                    }
+
+                    recordCount++;
+                }
+
+                // Should be two records, one for each session (but the first session has no clicks, so it shouldn't have any records)
+                Assert.AreEqual(2, recordCount);
+            });
+
+            task.Wait();
+        }
+
+        [Test]
         public void UnreductionResultsInNoDocuments()
         {
             // As for BrokenDownBy, but indexing by event type as well
