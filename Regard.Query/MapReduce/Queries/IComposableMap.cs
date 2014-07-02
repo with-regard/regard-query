ae29dd583493df3287a7801d701667b78e6fa416
Andrew Hunter <andrew.hunter@red-gate.com>
2014-07-02 11:49:50 +0100

    Add some documentation making it explicit that the input object can be modified by a map operation for composability reasons

--- Regard.Query/MapReduce/Queries/IComposableMap.cs
+++ Regard.Query/MapReduce/Queries/IComposableMap.cs
@@ -13,6 +13,11 @@ namespace Regard.Query.MapReduce.Queries
         /// <summary>
         /// Updates the MapResult on the basis of the data stored in the input object
         /// </summary>
+        /// <remarks>
+        /// Updating the input object is also allowed: this makes it possible to provide alternative views of the input to other 
+        /// composed map operations. This is useful for things like reformatting a date and then using the reformatted version to
+        /// break down the results.
+        /// </remarks>
         void Map(MapResult result, JObject input);
     }
 }
