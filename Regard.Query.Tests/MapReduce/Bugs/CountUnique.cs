d1a19f974c95410e2c38907906cf04abadb81ac6
Andrew Hunter <andrew.hunter@red-gate.com>
2014-07-14 17:04:22 +0100

    Add a new test class designed to look into the issue we're seeing with user counts going wrong.
    Right now, the test is failing as uningesting isn't working quite right.

---
+++ Regard.Query.Tests/MapReduce/Bugs/CountUnique.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Regard.Query.Api;
+using Regard.Query.MapReduce;
+using Regard.Query.Serializable;
+
+namespace Regard.Query.Tests.MapReduce.Bugs
+{
+    [TestFixture]
+    class CountUnique
+    {
+        /// <summary>
+        /// Performs various actions on the data store and tracks state so we can verify that they worked OK
+        /// </summary>
+        private class UserCreepTester
+        {
+            private readonly IKeyValueStore m_Store;
+            private readonly SerializableQuery m_Query; 
+            private DataIngestor m_Ingestor;
+
+            private Dictionary<string, List<JObject>> m_EventsForUser = new Dictionary<string, List<JObject>>();
+
+            public UserCreepTester(SerializableQuery query, IKeyValueStore store)
+            {
+                m_Store     = store;
+                m_Query     = query;
+                m_Ingestor  = new DataIngestor(query.GenerateMapReduce(), store);
+            }
+
+            /// <summary>
+            /// Creates a new user with no events
+            /// </summary>
+            public void CreateNewUser()
+            {
+                // An improvement would be to use a known GUID sequence
+                Guid someUserGuid = new Guid();
+                m_EventsForUser[someUserGuid.ToString()] = new List<JObject>();
+            }
+
+            public void RecreateIngestor()
+            {
+                m_Ingestor = new DataIngestor(m_Query.GenerateMapReduce(), m_Store);
+            }
+
+            public async Task AddEventsForAllUsers(int eventCount)
+            {
+                // Just add a bunch of events
+                foreach (string userId in m_EventsForUser.Keys)
+                {
+                    var userEvents = m_EventsForUser[userId];
+
+                    for (int anEvent = 0; anEvent < eventCount; ++anEvent)
+                    {
+                        JObject evtObject = new JObject();
+
+                        evtObject["user-id"]        = userId;
+                        evtObject["action"]         = "testing";
+                        evtObject["sequenceCount"]  = anEvent;
+
+                        m_Ingestor.Ingest(evtObject);
+                        userEvents.Add(evtObject);
+                    }
+                }
+
+                // Commit later on
+                await m_Ingestor.Commit();
+            }
+
+            public async Task DeleteEventsForSomeUser()
+            {
+                // Not especially random, but I don't think it matters for this test
+                var userId = m_EventsForUser.Keys.First();
+
+                var userEvents = m_EventsForUser[userId];
+
+                foreach (var evt in userEvents)
+                {
+                    m_Ingestor.Uningest(evt);
+                }
+
+                await m_Ingestor.Commit();
+            }
+
+            public async Task CheckUserCountIsRight()
+            {
+                // We should end up with a data store containing a single record that counts how many unique user IDs there are
+                var numUniqueUserIds    = m_EventsForUser.Count;
+                var values              = m_Store.EnumerateAllValues();
+                int numRecords          = 0;
+
+                for (var val = await values.FetchNext(); val != null; val = await values.FetchNext())
+                {
+                    ++numRecords;
+                    var count = val.Item2["value"].Value<int>();
+                    Assert.AreEqual(numUniqueUserIds, count);
+                }
+
+                Assert.AreEqual(1, numRecords);
+            }
+        }
+
+        [Test]
+        public void CountUniqueCreep()
+        {
+            Task.Run(async () =>
+            {
+                /// https://github.com/with-regard/regard-query/issues/1
+                var queryBuilder = new SerializableQueryBuilder(null);
+                var uniqueUsers = (SerializableQuery)queryBuilder.AllEvents().CountUniqueValues("user-id", "value");
+
+                // Assume that the bug isn't down to the data store but the map/reduce algorithm itself, so we'll do this in-memory for now
+                var resultStore = new MemoryKeyValueStore();
+                var tester = new UserCreepTester(uniqueUsers, resultStore);
+
+                // Two users
+                tester.CreateNewUser();
+                tester.CreateNewUser();
+
+                // Add a few events
+                await tester.AddEventsForAllUsers(5);
+                await tester.AddEventsForAllUsers(5);
+                await tester.AddEventsForAllUsers(5);
+                await tester.AddEventsForAllUsers(5);
+
+                // Check
+                await tester.CheckUserCountIsRight();
+
+                // Delete and check
+                await tester.DeleteEventsForSomeUser();
+                await tester.CheckUserCountIsRight();
+            }).Wait();
+        }
+    }
+}
